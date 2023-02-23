b3de2d6f3e5ea536358ba5f593ba52a188ee1475
Munkhtur <muujig1@gmail.com>
2023-02-24 00:00:38 +0800

    login

--- components/Login.vue
+++ components/Login.vue
@@ -1,15 +1,12 @@
 <template>
-<div class="button">
-  <button
-    @click="handleFacebookLogin"
-  >
-    Login with Facebook
-  </button>
-</div>
+  <div class="button">
+    <button v-if="isLoggedIn" @click="handleFacebookLogout">Logout</button>
+    <button v-else @click="handleFacebookLogin">Login with Facebook</button>
+  </div>
 </template>
 
 <script>
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, FacebookAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
@@ -24,16 +21,23 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const provider = new FacebookAuthProvider();
-
+var user1;
 export default {
   methods: {
+    isLoggedIn : function(){ console.log("Login>>",this.user1); return this.user1},
+    handleFacebookLogout(){
+      console.log("Logout>>",user1);
+    },
     handleFacebookLogin(path, data) {
       const auth = getAuth();
       signInWithPopup(auth, provider)
         .then((result) => {
           // The signed-in user info.
           const user = result.user;
-          console.log("=======>",user);
+          user1 = user;
+          console.log("=======>", user);
+          console.log("1=======>", user1);
+
           // This gives you a Facebook Access Token. You can use it to access the Facebook API.
           const credential = FacebookAuthProvider.credentialFromResult(result);
           const accessToken = credential.accessToken;
@@ -55,62 +59,11 @@ export default {
     },
   },
 };
-
-// methods: {
-//   handleFacebookLogin(path, data) {
-
-//   }
-// }
-
-// const handleFacebookLogin = () => {
-//   const provider = new firebase.auth.FacebookAuthProvider();
-
-//   firebase.auth()
-//           .signInWithPopup(provider)
-//           .then((result) => {
-//             var credential = result.credential;
-
-//             var token = credential.accessToken;
-
-//             var user = result.user;
-//           }).catch((error) => {
-//             var errorCode = error.code;
-//             var errorMessage = error.message;
-//             var email = error.email;
-//             var credential = error.credential;
-//           });
-// }
-// export default {
-//   const auth = getAuth();
-// signInWithPopup(auth, provider)
-//   .then((result) => {
-//     // The signed-in user info.
-//     const user = result.user;
-
-//     // This gives you a Facebook Access Token. You can use it to access the Facebook API.
-//     const credential = FacebookAuthProvider.credentialFromResult(result);
-//     const accessToken = credential.accessToken;
-
-//     // IdP data available using getAdditionalUserInfo(result)
-//     // ...
-//   })
-//   .catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.customData.email;
-//     // The AuthCredential type that was used.
-//     const credential = FacebookAuthProvider.credentialFromError(error);
-
-//     // ...
-//   });
-// };
 </script>
 
 <style>
-  .button {
-    position: absolute;
-    top: 100px;
-  }
+.button {
+  position: absolute;
+  top: 100px;
+}
 </style>
