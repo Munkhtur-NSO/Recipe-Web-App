a02dc0530e824a251c45aa2cfed21367de4fab86
munkhuu <Munkhtur@nso.mn>
2023-02-28 14:31:55 +0800

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/Recipe-Web-App/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'Recipe-Web-App',
